3d0733d1a9fb4dbda15167f86406fb78ac516ac2
wang qian <wang_qian0219@wangdeMacBook-Pro.local>
2023-04-04 00:09:10 +0800

    hello v0.1.0

---
+++ hello.go
@@ -0,0 +1,7 @@
+package HELLO
+
+import "rsc.io/quote"
+
+func Hello() string {
+	return quote.Hello()
+}
---
+++ hello_test.go
@@ -0,0 +1,10 @@
+package HELLO
+
+import "testing"
+
+func HelloTest(t *testing.T) {
+	want := "Hello World!"
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
